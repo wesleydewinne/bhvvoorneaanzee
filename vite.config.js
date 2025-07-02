653186038724fe4be040c22de65524e44a83ca7a
wesleydewinne <wesleydewinne@gmail.com>
2025-07-02 17:22:28 +0200

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // eslint-disable-next-line no-undef
   base: process.env.VITE_BASE_PATH || "/bhvvoorneaanzee"
 })
