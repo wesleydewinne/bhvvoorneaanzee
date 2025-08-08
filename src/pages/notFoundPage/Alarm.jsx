95e45057139c3bca2e8d6fa0c891bea664b01956
wesleydewinne <wesleydewinne@gmail.com>
2025-08-08 16:38:49 +0200

    Voeg diverse assets en notFoundPage componenten toe: animatie, afbeeldingen, geluid en componenten

---
+++ src/pages/notFoundPage/Alarm.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useRef, useState } from "react";
+import alarmSound from "../../assets/sound/alarm.mp3";
+
+function Alarm() {
+    const audioRef = useRef(null);
+    const [hasPlayed, setHasPlayed] = useState(false);
+
+    useEffect(() => {
+        const onFirstClick = () => {
+            if (audioRef.current && !hasPlayed) {
+                audioRef.current.play().catch(err => {
+                    console.warn("Play failed:", err);
+                });
+                setHasPlayed(true);
+            }
+            window.removeEventListener("click", onFirstClick);
+        };
+
+        window.addEventListener("click", onFirstClick);
+
+        return () => {
+            window.removeEventListener("click", onFirstClick);
+        };
+    }, [hasPlayed]);
+
+    return (
+        <audio ref={audioRef} src={alarmSound} loop preload="auto" />
+    );
+}
+
+export default Alarm;
---
+++ src/pages/notFoundPage/SmokeLottieOverlay.jsx
@@ -0,0 +1,28 @@
+// SmokeLottieOverlay.jsx
+import React from "react";
+import Lottie from "lottie-react";
+import smokeAnimation from "../../assets/animatie/smoke.json"; // pad naar jouw bestand
+
+const SmokeLottieOverlay = () => {
+    return (
+        <div style={{
+            top: 0,
+            left: 0,
+            pointerEvents: "none",
+            zIndex: 9999,
+            opacity: 0.7, // rook wat transparant maken
+        }}>
+            <Lottie
+                animationData={smokeAnimation}
+                loop
+                autoplay
+                style={{
+                    width: "100%",
+                    height: "100%",
+                }}
+            />
+        </div>
+    );
+};
+
+export default SmokeLottieOverlay;
