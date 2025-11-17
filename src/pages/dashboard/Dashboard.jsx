913203ce719698c5903890c8f46efc0aa19d2d23
wesleydewinne <wesleydewinne@gmail.com>
2025-11-17 20:09:45 +0100

    feat(dashboard): added minimal dashboard showing welcome text and username placeholder

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,10 +1,16 @@
 import './Dashboard.css';
 
 function Dashboard() {
+
+    // Later kun je deze data uit context, props of backend halen
+    const name = "Wesley";
+    const username = "wesley123";
+
     return (
-        <>
-            
-        </>
+        <div className="dashboard-container">
+            <h2>Welkom terug, {name}!</h2>
+            <p className="username">Ingelogd als: {username}</p>
+        </div>
     );
 }
 
