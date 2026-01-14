d2dfb4aaaae074e128f06872c612e6ebfeb73102
wesleydewinne <wesleydewinne@gmail.com>
2026-01-14 12:40:19 +0100

    fix check.css to Check.css

--- src/pages/leadPage/Check.jsx
+++ src/pages/leadPage/Check.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./check.css";
+import "./Check.css";
 import SafetyWizard from "./SafetyWizard";
 
 import bgImage from "@/assets/image/headerPicture/veiligheidscheck-bg.jpg";
--- src/pages/leadPage/VeiligheidscheckBedankt.jsx
+++ src/pages/leadPage/VeiligheidscheckBedankt.jsx
@@ -1,5 +1,4 @@
 import "./Check.css";
-import { Link } from "react-router-dom";
 
 export default function VeiligheidscheckBedankt() {
     return (
