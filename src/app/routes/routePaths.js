c707525fe7bb3a0482d33fa525b953f58a4d212a
wesleydewinne <wesleydewinne@gmail.com>
2026-03-15 23:56:38 +0100

    aanpassing json

--- src/app/routes/routePaths.js
+++ src/app/routes/routePaths.js
@@ -1,14 +1,7 @@
 // src/routes/routePaths.js
-export const PRIVATE_PATHS = {
-    dashboard: "/dashboard",
-    profile: "/profile",
-};
+export const PRIVATE_PATHS = ["/dashboard", "/profile"];
 
-export const QUIZ_PATHS = {
-    intro: "/quiz-intro",
-    quiz: "/quiz",
-    result: "/quiz-result",
-};
+export const QUIZ_PATHS = ["/quiz-intro", "/quiz", "/quiz-result"];
 
 export const ADMIN_PATHS = {
     evaluationQr: "/admin/evaluations/qr",
